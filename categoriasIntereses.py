d1a9d6ca8fa3f0e4e465f4b5f7e15f8f4248ce9b
EstebanManriqueL <82970873+EstebanManriqueL@users.noreply.github.com>
2021-08-09 09:26:17 -0500

    Coreccion nombre libreria IBM

--- categoriasIntereses.py
+++ categoriasIntereses.py
@@ -15,7 +15,7 @@ import os
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, SentimentOption
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
 
 #Autentificacion de usuario y selección de archivo/sheet
 def seleccion_Archivo(id_archivo):
